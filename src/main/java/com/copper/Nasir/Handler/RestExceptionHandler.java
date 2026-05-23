eb68e829fe3ffdc941884b213418e85c10107c04
Rodrigo Lima Barbosa12 <rodrigo.limabarbosa@upe.br>
2026-05-23 14:16:42 -0300

    Update RestExceptionHandler.java
    Sugestão de alteração:
    Faltou o .class no controlador de exceções do @ExceptionHandler e o método está vazio (sem retorno), isso não dá problema para compilar?

--- src/main/java/com/copper/Nasir/Handler/RestExceptionHandler.java
+++ src/main/java/com/copper/Nasir/Handler/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.copper.Nasir.Handler;
 
 import com.copper.Nasir.Exception.UserNotFoundException;
+// import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,3 +14,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     }
 }
+/// Sugestão: @ExceptionHandler(UserNotFoundException.class)
+    // public ResponseEntity<RestErrorMessage> userNotFoundExceptionHandler(UserNotFoundException exception) {
+       // RestErrorMessage errorResponse = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+       // return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
