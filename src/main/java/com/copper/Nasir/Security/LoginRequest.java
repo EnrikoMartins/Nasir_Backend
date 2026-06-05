ca6e5f2a4e947cd1e3460a2e83c6f828e7c315bd
Rodrigo Lima Barbosa12 <rodrigo.limabarbosa@upe.br>
2026-06-05 08:44:42 -0300

    feat(security): adicionar DTO LoginRequest comentado

---
+++ src/main/java/com/copper/Nasir/Security/LoginRequest.java
@@ -0,0 +1,23 @@
+package com.copper.Nasir.Security;
+
+/* import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail inválido")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    private String password;
+}
+*/
