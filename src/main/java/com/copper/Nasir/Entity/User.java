5f067b6328667ba66073c4b66120d4efbd17a2d0
Rodrigo Lima Barbosa12 <rodrigo.limabarbosa@upe.br>
2026-06-05 08:33:32 -0300

    Update User.java
    Sugestões de Alteração

--- src/main/java/com/copper/Nasir/Entity/User.java
+++ src/main/java/com/copper/Nasir/Entity/User.java
@@ -4,25 +4,69 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.userdetails.UserDetails;
 
+// import java.util.Collection;
+// import java.util.List;
 import java.util.UUID;
 
 @Entity
+// @Table(name = "tb_user")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+// @Builder
+public class User { // public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
     @Email
     @NotNull
-    @Column(nullable = false)
+    @Column(nullable = false) // @Column(nullable = false, unique = true)
     private String email;
+    
     @NotNull
     @Column(nullable = false)
     private String password;
+
+    // Métodos da interface UserDetails
+    //@Override
+    //public Collection<? extends GrantedAuthority> getAuthorities() {
+        //return List.of(() -> "ROLE_USER");
+    //}
+
+    //@Override
+    //public String getUsername() {
+        //return this.email;
+    //}
+
+    //@Override
+    //public String getPassword() {
+        //return this.password;
+    //}
+
+    //@Override
+    //public boolean isAccountNonExpired() {
+        //return true;
+    //}
+
+    //@Override
+    //public boolean isAccountNonLocked() {
+        //return true;
+    //}
+
+    //@Override
+    //public boolean isCredentialsNonExpired() {
+        //return true;
+    //}
+
+    //@Override
+    //public boolean isEnabled() {
+        //return true;
+    //}
 
 
 }
