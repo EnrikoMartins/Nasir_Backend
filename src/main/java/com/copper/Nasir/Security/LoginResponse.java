633e306a5b5f7f5eefc01a36ebee7d6dac13c039
Rodrigo Lima Barbosa12 <rodrigo.limabarbosa@upe.br>
2026-06-05 08:46:10 -0300

    feat(security): adicionar DTO LoginResponse comentado

---
+++ src/main/java/com/copper/Nasir/Security/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.copper.Nasir.Security;
+
+/* import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    
+    private String token;
+}
+*/
